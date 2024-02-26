e28216419db8183ab7332a4a641b465ccec5c786
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-26 18:53:30 -0500

    Fixed reverse_linked_list.py

--- src/linked-lists/reverse_linked_list.py
+++ src/linked-lists/reverse_linked_list.py
@@ -1,26 +1,26 @@
 class Node:
-    """"""
+    """Node class for a linked list."""
 
     def __init__(self, data: int) -> None:
-        """
+        """Initialize a node with data.
 
-        :param data:
+        :param data: Data to be stored in the node.
         """
         self.data = data
         self.next = None
 
 
 class LinkedList:
-    """"""
+    """Linked list implementation."""
 
     def __init__(self) -> None:
-        """"""
+        """Initialize an empty linked list."""
         self.head = None
 
     def __str__(self) -> str:
-        """
+        """Return a string representation of the linked list.
 
-        :return:
+        :return: String representation of the linked list.
         """
         current, string = self.head, ""
 
@@ -32,18 +32,14 @@ class LinkedList:
         return string
 
     def populate(self) -> None:
-        """
-
-        :return:
-        """
+        """Populate the linked list with sample data."""
         for i in range(1, 6):
             self.append(i)
 
     def append(self, num: int) -> None:
-        """
+        """Append a node with given data to the end of the linked list.
 
-        :param num:
-        :return:
+        :param num: Data to be appended to the linked list.
         """
         new_node = Node(num)
 
@@ -54,15 +50,12 @@ class LinkedList:
         current = self.head
         while current.next:
             current = current.next
-
         current.next = new_node
 
     def reverse(self) -> None:
-        """
-
-        :return:
-        """
+        """Reverse the linked list in-place."""
         previous, current = None, self.head
+
         while current:
             next_node = current.next
             current.next = previous
