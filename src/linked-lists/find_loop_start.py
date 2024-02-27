037b760ba9b5c91cd6e0be7b43dba7ae94021323
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-27 10:56:52 -0500

    Fixed find_loop_start.py

--- src/linked-lists/find_loop_start.py
+++ src/linked-lists/find_loop_start.py
@@ -27,50 +27,41 @@ class LinkedList:
         for i in range(1, 7):
             string += f"{current.data} -> "
             current = current.next
-        string += "3"
+
+        # Make Node 3 the "last node" for the string representation
+        string += f"{current.data}"
 
         return string
 
     def populate(self) -> None:
-        """Populate the linked list with sample data."""
+        """Populate the linked list with sample data and create a loop."""
         self.head = Node(1)
         current, loop_start = self.head, None
 
         for i in range(2, 7):
             current.next = Node(i)
+            current = current.next
 
             if i == 3:
-                loop_start = current.next
+                loop_start = current
 
-            current = current.next
-
+        # Create the loop from Node 6 to Node 3
         current.next = loop_start
 
-    def create_loop(self, num: int) -> None:
-        """Append a node with given data to the end of the linked list.
-
-        :param num: Data to be appended to the linked list.
-        """
-        new_node = Node(num)
-
-        if not self.head:
-            self.head = new_node
-            return
-
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = new_node
-
     def find_loop_start(self) -> Node:
-        """Reverse the linked list in-place."""
-        slow, fast = self.head, self.head
+        """Find the start node of the loop in the linked list.
 
-        while fast is not None and fast.next is not None:
+        :return: The start node of the loop if found, None otherwise.
+        """
+        slow = fast = self.head
+
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
+            # Loop is found.
             if slow == fast:
+                # Reset the slow pointer back at the head
                 slow = self.head
 
                 while slow != fast:
@@ -90,8 +81,11 @@ def main() -> None:
     print("Linked List")
     print(linked_list)
 
-    loop_start_node_num = linked_list.find_loop_start().data
-    print(f"\nThe loop starts at Node {loop_start_node_num}!")
+    loop_start_node = linked_list.find_loop_start()
+    if loop_start_node:
+        print(f"\nThe loop starts at Node {loop_start_node.data}!")
+    else:
+        print("\nNo loop found in the linked list.")
 
 
 if __name__ == "__main__":
