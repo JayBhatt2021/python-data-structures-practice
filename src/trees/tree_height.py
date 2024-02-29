2f61bad1a54a5e574beda101fd779934017e855c
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-29 13:56:19 -0500

    Tweaked tree_height.py

--- src/trees/tree_height.py
+++ src/trees/tree_height.py
@@ -2,16 +2,16 @@ class Node:
     """Node class for a binary tree."""
 
     def __init__(
-            self,
-            data: int,
-            left: 'Node' = None,
-            right: 'Node' = None,
+        self,
+        data: int,
+        left: "Node" = None,
+        right: "Node" = None,
     ) -> None:
         """Initialize a node with data.
 
         :param data: Data to be stored in the node.
-        :param left:
-        :param right:
+        :param left: Left child node.
+        :param right: Right child node.
         """
         self.data = data
         self.left = left
@@ -30,14 +30,21 @@ class BinaryTree:
         self.root = Node(1, Node(2, Node(3, right=Node(4)), Node(5)), Node(6))
 
     def height(self, node: Node) -> int:
-        """Find the start node of the loop in the linked list.
+        """Calculate the height of the binary tree.
 
-        :return: The start node of the loop if found, None otherwise.
+        :param node: The root node of the binary tree.
+        :return: The height of the binary tree.
         """
+        # Base case: The height of a "None" node is 0.
         if not node:
             return 0
 
-        return 1 + max(self.height(node.left), self.height(node.right))
+        left_height = self.height(node.left)
+        right_height = self.height(node.right)
+
+        # Recursive step: The height of the tree is 1 (the height of the current
+        # node) plus the height of the tallest child.
+        return 1 + max(left_height, right_height)
 
 
 def main() -> None:
