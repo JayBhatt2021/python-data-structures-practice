af6cd43e232e7af98af80162d12235ca213b891f
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-29 17:25:47 -0500

    Renamed smallest_integer_remover.py to alternate_smallest_integers.py

--- src/heaps/alternate_smallest_integers.py
+++ src/heaps/alternate_smallest_integers.py
@@ -14,18 +14,21 @@ def generate_random_integers(lower: int, upper: int, length: int) -> List[int]:
     return [random.randint(lower, upper) for _ in range(length)]
 
 
-def remove_smallest_integers(integers: List[int]) -> List[int]:
-    """
+def alternate_smallest_integers(integers: List[int]) -> List[int]:
+    """Alternately arrange the smallest integers from the input list.
 
-    :param integers:
-    :return:
+    :param integers: A list of integers.
+    :return: A list of integers with the smallest integers arranged alternately.
     """
     heapq.heapify(integers)
     smallest_integers = []
 
     while integers:
+        # Pop the smallest and second-smallest integers from the heap
         smallest = heapq.heappop(integers)
         second_smallest = heapq.heappop(integers)
+
+        # Append the second_smallest and smallest to smallest_integers
         smallest_integers.append(second_smallest)
         smallest_integers.append(smallest)
 
@@ -34,8 +37,8 @@ def remove_smallest_integers(integers: List[int]) -> List[int]:
 
 def main() -> None:
     """Main function."""
-    random_ints = generate_random_integers(-100, 100, 20)
-    print(f"Smallest Integers: {remove_smallest_integers(random_ints)}")
+    random_ints = generate_random_integers(-100, 100, 6)
+    print(f"Smallest Integers: {alternate_smallest_integers(random_ints)}")
 
 
 if __name__ == "__main__":
