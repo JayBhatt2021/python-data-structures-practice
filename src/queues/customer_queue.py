c698acb4b4867a540d885f252df6cf835a5a1f71
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-28 19:43:24 -0500

    Fixed customer_queue.py

--- src/queues/customer_queue.py
+++ src/queues/customer_queue.py
@@ -2,7 +2,7 @@ import random
 from typing import List
 
 
-def generate_random_numbers(lower: int, upper: int, length: int) -> List[int]:
+def generate_random_integers(lower: int, upper: int, length: int) -> List[int]:
     """Generate a list of random integers within a specified range.
 
     :param lower: The lower bound of the range.
@@ -14,37 +14,50 @@ def generate_random_numbers(lower: int, upper: int, length: int) -> List[int]:
 
 
 def count_hungry_customers(
-        cookie_stack: List[int], customer_queue: List[int]
+    cookie_stack: List[int], customer_queue: List[int]
 ) -> int:
-    """Count the number of hungry customers based on cookie stack and customer
-    queue.
+    """Count the number of hungry customers based on the cookie stack and
+    customer queue.
 
-    :param cookie_stack: List representing the stack of available cookies.
-    :param customer_queue: List representing the queue of hungry customers.
+    :param cookie_stack: List representing the stack of cookies.
+    :param customer_queue: List representing the queue of customers.
     :return: The number of customers who couldn't get a cookie.
     """
+    # One-to-one correspondence between cookie_stack and customer_queue will
+    # result in 0 hungry customers.
     if cookie_stack == customer_queue:
         return 0
 
-    customer_frequency = {c: 0 for c in set(customer_queue)}
+    # Initialize the count of each type of customer to 0
+    # (It has to be taken from the set of the elements in cookie_stack PLUS
+    # customer_queue because set(cookie_stack) may not necessarily equal
+    # set(customer_queue). This is done to avoid a KeyError when accessing
+    # customer_frequency.)
+    customer_frequency = {c: 0 for c in set(cookie_stack + customer_queue)}
 
+    # Obtain the count of each type of customer
     for c in customer_queue:
         customer_frequency[c] += 1
 
+    # Reduce the number of hungry customers based on the cookie that is on top
+    # of the stack
+    # (If there are no customers that can eat the cookie on the top of the
+    # stack, then the current customer onwards will go hungry.)
     for c in cookie_stack:
         if customer_frequency[c]:
             customer_frequency[c] -= 1
         else:
             break
 
+    # Return the total number of hungry customers
     return sum(customer_frequency.values())
 
 
 def main() -> None:
     """Main function."""
     lower, upper, length = 0, 1, 10
-    cookie_stack = generate_random_numbers(lower, upper, length)
-    customer_queue = generate_random_numbers(lower, upper, length)
+    cookie_stack = generate_random_integers(lower, upper, length)
+    customer_queue = generate_random_integers(lower, upper, length)
 
     print(f"Cookie Stack: {cookie_stack}")
     print(f"Customer Queue: {customer_queue}")
