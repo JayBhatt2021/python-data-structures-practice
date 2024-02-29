e0ba20c86e6511558ceb8fc66e4c03080593db69
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-29 16:50:10 -0500

    Fixed tree_equality.py

--- src/trees/tree_equality.py
+++ src/trees/tree_equality.py
@@ -2,10 +2,10 @@ class Node:
     """Node class for a binary tree."""
 
     def __init__(
-            self,
-            data: int,
-            left: "Node" = None,
-            right: "Node" = None,
+        self,
+        data: int,
+        left: "Node" = None,
+        right: "Node" = None,
     ) -> None:
         """Initialize a node with data.
 
@@ -26,35 +26,40 @@ class BinaryTree:
         self.root = None
 
     def __eq__(self, other: object) -> bool:
-        """
+        """Check if two binary trees are equal.
 
-        :param other:
-        :return:
+        :param other: Another object to compare.
+        :return: True if the trees are equal, False otherwise.
         """
         return isinstance(other, BinaryTree) and self._are_trees_equal(
-            self.root, other.root)
+            self.root, other.root
+        )
 
     def populate(self) -> None:
         """Populate the binary tree with sample data."""
         self.root = Node(9, right=Node(5, Node(3, Node(11), Node(7))))
 
-    def _are_trees_equal(self, self_root, other_root: Node) -> bool:
-        """
+    def _are_trees_equal(self, self_root: Node, other_root: Node) -> bool:
+        """Check if two trees are equal recursively.
 
-        :param self_root:
-        :param other_root:
-        :return:
+        :param self_root: Root of the first tree.
+        :param other_root: Root of the second tree.
+        :return: True if the trees are equal, False otherwise.
         """
+        # Base case #1: Both nodes are "None".
         if not self_root and not other_root:
             return True
 
+        # Base case #2: One node is "None" while the other node is not "None".
         if not self_root or not other_root:
             return False
 
+        # Recursive step: The two trees are equal if their roots, left subtrees,
+        # and right subtrees are equal.
         return (
-                self_root.data == other_root.data and
-                self._are_trees_equal(self_root.left, other_root.left) and
-                self._are_trees_equal(self_root.right, other_root.right)
+            self_root.data == other_root.data
+            and self._are_trees_equal(self_root.left, other_root.left)
+            and self._are_trees_equal(self_root.right, other_root.right)
         )
 
 
