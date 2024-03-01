230d7d6fb867310144d29eecd0ef925c60cc3c95
Jay Bhatt <jay.bhatt98@gmail.com>
2024-03-01 17:06:06 -0500

    Fixed largest_list_elements.py

--- src/heaps/largest_list_elements.py
+++ src/heaps/largest_list_elements.py
@@ -4,53 +4,51 @@ from typing import List
 
 
 class LargestListElements:
-    """Replace this"""
+    """A class designed to efficiently manage and retrieve the largest elements
+    up to a specified position within a list.
+    """
 
     def __init__(self, elements: List[int], position: int) -> None:
+        """Initialize an instance of LargestListElements.
+
+        :param elements: The list of elements.
+        :param position: The position to track the largest elements.
+        :raise ValueError: If the position is invalid.
         """
-
-        :param elements:
-        :param position:
-        :return:
-        :raise Exception:
-        """
-        if not elements:
-            raise Exception("The element list must be populated with elements!")
-
-        if position < 1:
-            raise Exception(
-                'Returning "0th" (or lower) largest element does not make '
-                'logical sense.'
-            )
-
-        if position > len(elements):
-            raise Exception(
-                "The position cannot exceed the length of the element list!"
-            )
+        # Obtaining the "0th" (or lower) largest element doesn't make logical
+        # sense. Additionally, the position has to be at most len(elements) + 1
+        # to prevent an IndexError from occurring in the append method.
+        if position < 1 or position > len(elements) + 1:
+            raise ValueError("Invalid value for the position!")
 
         self.min_heap = elements
         self.position = position
 
+        # Sort the elements from smallest to largest
         heapq.heapify(self.min_heap)
 
+        # Remove any elements that are smaller than the "position"th largest
+        # element
         while len(self.min_heap) > position:
             heapq.heappop(self.min_heap)
 
     def __str__(self) -> str:
-        """
+        """Return the string representation of LargestListElements.
 
-        :return:
+        :return: The string representation of LargestListElements.
         """
         return str(self.min_heap)
 
     def append(self, element: int) -> int:
-        """
+        """Append an element to the min heap and maintain the "position" largest
+        elements.
 
-        :param element:
-        :return:
+        :param element: The element to append.
+        :return: The "position"th largest element after appending.
         """
         heapq.heappush(self.min_heap, element)
 
+        # Maintain the "position" largest elements in the min heap
         if len(self.min_heap) > self.position:
             heapq.heappop(self.min_heap)
 
@@ -77,11 +75,13 @@ def main() -> None:
     append_elements = generate_random_integers(-40, 20, 3)
     for i, num in enumerate(append_elements):
         print(f"Appending {num} to {largest_list_elements}...")
-        position_element = largest_list_elements.append(num)
+        position_largest_element = largest_list_elements.append(num)
+
         print(
             f"The {position}th largest element in {largest_list_elements} is "
-            f"{position_element}."
+            f"{position_largest_element}."
         )
+
         if i != len(append_elements) - 1:
             print()
 
